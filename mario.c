ef10fc5baba4b33a07f9e1e4cf44d15148563c13
Andrew Szabo <83946081+andr3wpex@users.noreply.github.com>
2022-10-09 16:04:17 +0200

    mario.c added with pseudo code

---
+++ mario.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // 1 ask for user_input
+
+    // 2 if user input is integer and between 1 and 8 continue else go back to 1
+
+    // 3 print shape with the specified height
+
+    return(0);
+}
