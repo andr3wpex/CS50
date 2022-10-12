060fbcdfe9aa93be812d0fbfb860b91fc610d5db
Andrew Szabo <83946081+andr3wpex@users.noreply.github.com>
2022-10-12 18:55:45 +0200

    error fixed when user provided number > 8

--- mario.c
+++ mario.c
@@ -9,33 +9,33 @@ int main()
         printf("Please select height (1 to 8):\n");
         scanf("%d", &height);
         
-        for (i = 1; i <= height; i++)
-        {
-        
-        // left side
-            for (j = 1; j <= height - i; j++)
-            {
-                printf(" ");
-            }
-            for (k = 1; k <= i; k++)
-            {
-                printf("#");
-            }
-        
-        // gap
-        printf("  ");
-        
-        // right side
-        for (j = 1; j <= i; j++)
-        {
-            printf("#");
-        }
-        
-        // next line
-        printf("\n");
-        }
     }
     while (height < 1 || height > 8);
     
+    for (i = 1; i <= height; i++)
+    {
+    
+    // left side
+        for (j = 1; j <= height - i; j++)
+        {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+    
+    // gap
+    printf("  ");
+    
+    // right side
+    for (j = 1; j <= i; j++)
+    {
+        printf("#");
+    }
+    
+    // next line
+    printf("\n");
+    }
     return (0);
 }
